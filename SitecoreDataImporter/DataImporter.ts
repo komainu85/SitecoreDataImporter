3cde9f288a3f1dee9aca77b76fbeecb9f719c2a3
sobek1985 <sobek1985@gmail.com>
2015-03-12 12:52:20 +0000

    Roll back to SPEAK 1.1

--- SitecoreDataImporter/DataImporter.ts
+++
@@ -1,9 +0,0 @@
-﻿import Speak = require("sitecore/shell/client/Speak/Assets/lib/core/1.2/SitecoreSpeak");
-
-class Dataimporter extends Speak.PageCode {
-    initialize() {
-        alert("hello World");
-    }
-};
-
-Sitecore.Speak.pageCode(["bootstrap", "scPipeline"], new Dataimporter());
