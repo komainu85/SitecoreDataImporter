9f868410da681651f7546dc802bdeb953adae846
sobek1985 <sobek1985@gmail.com>
2015-07-22 13:39:40 +0100

    Xml Parser

--- SitecoreDataImporter/Parsers/XmlParser.cs
+++ SitecoreDataImporter/Parsers/XmlParser.cs
@@ -1,20 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using MikeRobbins.SitecoreDataImporter.Entities;
 using MikeRobbins.SitecoreDataImporter.Interfaces;
 using Sitecore.Data.Items;
 
 namespace MikeRobbins.SitecoreDataImporter.Parsers
 {
-  public  class XmlParser : IParser
+    public class XmlParser : IParser
     {
-      public MediaItem MediaFile { get; set; }
-      public List<ImportItem> ParseMediaItem()
-      {
-          throw new NotImplementedException();
-      }
+        public MediaItem MediaFile { get; set; }
+
+        public List<ImportItem> ParseMediaItem()
+        {
+            List<ImportItem> items;
+
+            var document = DeserialiseXml();
+
+            items = CreateImportItem(document);
+
+            return items;
+        }
+
+        private XmlDocument DeserialiseXml()
+        {
+            XmlDocument document = new XmlDocument();
+
+            using (var sr = new StreamReader(MediaFile.GetMediaStream()))
+            {
+                using (XmlReader reader = XmlReader.Create(sr))
+                {
+                    document.Load(sr);
+                }
+            }
+
+            return document;
+        }
+
+        private List<ImportItem> CreateImportItem(XmlDocument document)
+        {
+            var items = new List<ImportItem>();
+
+            foreach (XmlNode node in document.ChildNodes)
+            {
+                var item = new ImportItem() { Title = MediaFile.Name };
+
+                if (node.Name.ToLower() == "title")
+                {
+                    item.Title = node.Value;
+                }
+
+                item.Fields.Add(node.Name, node.Value);
+
+                items.Add(item);
+
+            }
+
+            return items;
+        }
     }
 }
+
